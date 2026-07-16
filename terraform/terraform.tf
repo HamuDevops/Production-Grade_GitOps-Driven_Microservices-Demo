a4bcf9887ae3fc13360f4f5ce21435f8b584ae5d
Narwade <757150@cognizant.com>
2026-07-16 22:39:16 +0530

    Initial commit - GitOps Microservices Demo

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket         = "s3-tf-backup-2026"
+    key            = "s3-backend"
+    region         = "us-east-1"
+    
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.28.0"
+      version = ">= 6.52.0"
     }
   }
 }
